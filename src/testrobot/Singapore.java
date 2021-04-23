f371e1bb8a7aab04168b0e0d74b5edf91471210f
az242 <lifedistroy123456789@gmail.com>
2021-04-23 11:55:29 -0400

    consolidated scripts

--- src/testrobot/Singapore.java
+++ src/testrobot/Singapore.java
@@ -9,9 +9,23 @@ import java.io.IOException;
 import java.time.LocalTime;
 
 public class Singapore extends BaseBot{
+	MinimapData gs2Dungeon = new MinimapData("Gs2 Dungeon", new Rectangle(34,72,121,53), new Rectangle(47,45,135,15),"minimapNames/gs2dungeon.png");
+	MinimapData gs2 = new MinimapData("gs2", new Rectangle(6,72,121,53), new Rectangle(47,45,75,15),"minimapNames/gs2mapName.png");
+	MinimapData gs1 = new MinimapData("gs1", new Rectangle(6,72,115,53), new Rectangle(47,45,75,15),"minimapNames/gs1mapName.png");
+	
 	MinimapData ulu2 = new MinimapData("ulu2", new Rectangle(6,72,132,81), new Rectangle(47,45,70,15),"minimapNames/ulu2Mapname.png");
+	
+	Zone topFloor = new Zone(new MaplePoint(2,20),new MaplePoint(140,33));
+	Zone midFloor = new Zone(new MaplePoint(2,37),new MaplePoint(140,54));
+	Zone botFloor = new Zone(new MaplePoint(2,58),new MaplePoint(140,75));
+	MinimapData cd = new MinimapData("cds", new Rectangle(33,72,141,80), new Rectangle(47,45,115,15),"minimapNames/cdName.png");
+	
+	long dropTimer = 0;
 	public Singapore(Robot robot, Screen[] screens) {
 		super(robot, screens);
+		minimapDatas.add(gs2Dungeon);
+		minimapDatas.add(gs2);
+		minimapDatas.add(gs1);
 		minimapDatas.add(ulu2);
 		adjustMinimapData(mapleScreen,minimapDatas);
 	}
@@ -49,6 +63,8 @@ public class Singapore extends BaseBot{
 		switch(map.name) {
 		case "ulu2":
 			return ulu2(position, map);
+		case "gs2":
+			return GS2Movement(position, map);
 		}
 		return 0;
 	}
@@ -150,5 +166,207 @@ public class Singapore extends BaseBot{
 		robot.keyPress(KeyEvent.VK_C);
 		robot.delay(2750);
 		robot.keyRelease(KeyEvent.VK_C);
+	}
+	public int GS2Movement(int position, MinimapData map) throws IOException {
+		Zone leftSide = new Zone(new MaplePoint(40,2),new MaplePoint(52,45));
+		Zone rightSide = new Zone(new MaplePoint(80,23),new MaplePoint(92,45));
+		long currTime = System.currentTimeMillis();
+		if(currTime > dropTimer + 180*1000 && position == 1) {
+			dropTimer = currTime;
+			swapPlatforms(map);
+			moveToZoneX(leftSide, map);
+		}
+		MaplePoint currPos = getMinimapPosition(map);
+		if(position == 0) {
+			moveToZoneX(leftSide, map);
+			MaplePoint newPos = getMinimapPosition(map);
+			botOutput("Move from " + currPos.toString() + " to " + newPos.toString());
+			HolySymbol();
+			return 1;
+		} else{
+			moveToZoneX(rightSide, map);
+			MaplePoint newPos = getMinimapPosition(map);
+			botOutput("Move from " + currPos.toString() + " to " + newPos.toString());
+			return 0;
+		}
+	}
+	public void HolySymbol() {
+		long temptime = System.currentTimeMillis();
+		if(temptime >= buffTimers[0] + buffs[0].buffLength*1000) {
+			botOutput("Reapplying " + buffs[0].buffName);
+			buffTimers[0] = temptime;
+			keyPress(buffs[0].buffKey);
+			robot.delay(randomNum(1750,2500));
+		}
+	}
+	public void swapPlatforms(MinimapData map) throws IOException {
+		Zone dropdownZone = new Zone(new MaplePoint(90,25),new MaplePoint(120,32)); //dropdown
+		Zone teleUpZone = new Zone(new MaplePoint(34,40),new MaplePoint(40,45)); //teleup
+		
+		Zone droppedZone = new Zone(new MaplePoint(90,40),new MaplePoint(120,50)); //dropdown
+		Zone teledZone = new Zone(new MaplePoint(10,25),new MaplePoint(40,32)); //teleup
+		MaplePoint startCoords = getMinimapPosition(map);
+		if(dropdownZone.isInYZone(startCoords)) {
+			MaplePoint currentCoords = getMinimapPosition(map);
+			int retries = 0;
+			while(!droppedZone.isInYZone(currentCoords) && retries < 5){
+				botOutput("Trying to drop down...");
+				waitOnChat();
+				moveToZoneX(dropdownZone,map);
+				jumpDown();
+				currentCoords = getMinimapPosition(map);
+				retries++;
+			}
+			if(retries == 5) {
+				botOutput("Failed to drop. Exiting...");
+				exitScript();
+			} else {
+				botOutput("Move from " + startCoords.toString() + " to " + currentCoords.toString());
+			}
+		}else if(teleUpZone.isInYZone(startCoords)){
+			MaplePoint currentCoords = getMinimapPosition(map);
+			int retries = 0;
+			while(!teledZone.isInYZone(currentCoords) && retries < 5){
+				botOutput("Trying to tele up...");
+				waitOnChat();
+				moveToZoneX(teleUpZone,map);
+				teleportUp();
+				teleportUp();
+				currentCoords = getMinimapPosition(map);
+				retries++;
+			}
+			if(retries == 5) {
+				botOutput("Failed to teleport up. Exiting...");
+				exitScript();
+			} else {
+				botOutput("Move from " + startCoords.toString() + " to " + currentCoords.toString());
+			}
+		}
+	}
+	public int cdMovementAlone(int position, MinimapData map) throws IOException {
+		Zone attack1 = new Zone(new MaplePoint(27,20), new MaplePoint(32,75));
+		Zone attack2 = new Zone(new MaplePoint(65,20), new MaplePoint(69,75));
+		Zone attack3 = new Zone(new MaplePoint(110,20), new MaplePoint(114,75));
+		MaplePoint pos = getMinimapPosition(map);
+		switch(position) {
+		case 0:
+			if(!topFloor.isInZone(pos)) {
+				if(midFloor.isInZone(pos)){
+					midToTop(map);
+				}else {
+					botToMid(map);
+					midToTop(map);
+				}
+			}
+			moveToZoneX(attack1,map);
+			return position+1;
+		case 1:
+			moveToZoneX(attack2,map);
+			rebuff(.5);
+			return position+1;
+		case 2:
+			moveToZoneX(attack3,map);
+			return position+1;
+		case 3:
+			while(!midFloor.isInZone(pos)) {
+				jumpDown();
+				pos = getMinimapPosition(map);
+			}
+			moveToZoneX(attack3,map);
+			return position+1;
+		case 4:
+			moveToZoneX(attack2,map);
+			return position+1;
+		case 5:
+			moveToZoneX(attack1,map);
+			return position+1;
+		case 6:
+			while(!botFloor.isInZone(pos)) {
+				jumpDown();
+				pos = getMinimapPosition(map);
+			}
+			moveToZoneX(attack1,map);
+			return position+1;
+		case 7:
+			moveToZoneX(attack2,map);
+			return position+1;
+		case 8:
+			moveToZoneX(attack3,map);
+			return 0;
+		}
+		return 0;
+	}
+	public int cdMovement(int position, MinimapData map) throws IOException{
+		Zone attack1 = new Zone(new MaplePoint(27,20), new MaplePoint(32,75));
+		Zone attack2 = new Zone(new MaplePoint(65,20), new MaplePoint(69,75));
+		Zone attack3 = new Zone(new MaplePoint(110,20), new MaplePoint(114,75));
+		MaplePoint pos = getMinimapPosition(map);
+		switch(position) {
+		case 0:
+			if(!topFloor.isInZone(pos)) {
+				if(midFloor.isInZone(pos)){
+					midToTop(map);
+				}else {
+					botToMid(map);
+					midToTop(map);
+				}
+			}
+			moveToZoneX(attack1,map);
+			return position+1;
+		case 1:
+			moveToZoneX(attack2,map);
+			rebuff(.5);
+			return position+1;
+		case 2:
+			moveToZoneX(attack3,map);
+			return position+1;
+		case 3:
+			while(!midFloor.isInZone(pos)) {
+				jumpDown();
+				pos = getMinimapPosition(map);
+			}
+			moveToZoneX(attack3,map);
+			return position+1;
+		case 4:
+			moveToZoneX(attack2,map);
+			return position+1;
+		case 5:
+			moveToZoneX(attack1,map);
+			return position+1;
+		case 6:
+			while(!botFloor.isInZone(pos)) {
+				jumpDown();
+				pos = getMinimapPosition(map);
+			}
+			moveToZoneX(attack1,map);
+			return position+1;
+		case 7:
+			moveToZoneX(attack2,map);
+			return position+1;
+		case 8:
+			moveToZoneX(attack3,map);
+			return 0;
+		}
+		return 0;
+	}
+	public void botToMid(MinimapData map) throws IOException {
+		Zone tele = new Zone(new MaplePoint(44,65), new MaplePoint(45,70));
+		MaplePoint currPos = getMinimapPosition(map);
+		while(!midFloor.isInZone(currPos)){
+			moveToZoneX(tele, map);
+			keyPress(KeyEvent.VK_UP);
+			robot.delay(300);
+			currPos = getMinimapPosition(map);
+		}
+	}
+	public void midToTop(MinimapData map) throws IOException{
+		Zone tele = new Zone(new MaplePoint(6,45), new MaplePoint(7,51));
+		MaplePoint currPos = getMinimapPosition(map);
+		while(!topFloor.isInZone(currPos)){
+			moveToZoneX(tele, map);
+			keyPress(KeyEvent.VK_UP);
+			robot.delay(300);
+			currPos = getMinimapPosition(map);
+		}
 	}
 }
--- src/testrobot/Victoria.java
+++ src/testrobot/Victoria.java
@@ -8,10 +8,15 @@ import java.util.ArrayList;
 
 public class Victoria extends BaseBot{
 	MinimapData ironboar = new MinimapData("Iron Boar", new Rectangle(6,72,154,79), new Rectangle(47,45,65,15),"minimapNames/ironboarName.png");
+	MinimapData bubbling = new MinimapData("Bubbling", new Rectangle(6,72,259,79), new Rectangle(47,45,85,15),"minimapNames/bubblingName.png");
+	MinimapData jrwraith = new MinimapData("jrwraith", new Rectangle(6,72,259,79), new Rectangle(47,45,85,15),"minimapNames/jrwraithName.png");
+	
 	long dropTimer = 0;
 	public Victoria(Robot robot, Screen[] screens) {
 		super(robot, screens);
 		minimapDatas.add(ironboar);
+		minimapDatas.add(bubbling);
+		minimapDatas.add(jrwraith);
 		adjustMinimapData(mapleScreen,minimapDatas);
 	}
 	
@@ -28,13 +33,24 @@ public class Victoria extends BaseBot{
 		while(startTime + (hours * 60 * 60 * 1000) > currTime) {
 			System.out.println("<--------------->");
 			checkPots();
-			position = ironBoarMovement(position, map);
+			position = movement(position, map);
 			botOutput("Moved to position index: " + position);
 			robot.delay(400);
 			attack(1, KeyEvent.VK_C, 2750);
 			feedPets();
 		}
 		exitScript();
+	}
+	public int movement(int position, MinimapData map) throws IOException {
+		switch(map.name) {
+		case "Iron Boar":
+			return ironBoarMovement(position, map);
+		case "Bubbling":
+			return bubblingMovement(position, map);
+		case "jrwraith":
+			return jrWraithMovement(position, map);
+		}
+		return 0;
 	}
 	public int ironBoarMovement(int position, MinimapData map) throws IOException {
 		MaplePoint pos = getMinimapPosition(map);
@@ -71,6 +87,129 @@ public class Victoria extends BaseBot{
 			return 0;
 		}
 		
+		return 0;
+	}
+	public int bubblingMovement(int position, MinimapData map) throws IOException {
+		MaplePoint pos = getMinimapPosition(map);
+		switch(position){
+		case 8:
+		case 0:
+			Zone att1 = new Zone(new MaplePoint(60,37), new MaplePoint(63,44));
+			while(!att1.isInZone(pos)) {
+				moveToZoneX(att1, map);
+				if(!att1.isInYZone(pos))
+					teleportUp();
+				pos = getMinimapPosition(map);
+			}
+			return position + 1;
+		case 7:
+		case 1:
+			Zone att2 = new Zone(new MaplePoint(122,37), new MaplePoint(132,44));
+			while(!att2.isInZone(pos)) {
+				moveToZoneX(att2, map);
+				if(!att2.isInYZone(pos))
+					teleportUp();
+				pos = getMinimapPosition(map);
+			}
+			return position + 1;
+		case 6:
+		case 2:
+			Zone att3 = new Zone(new MaplePoint(162,37), new MaplePoint(167,44));
+			while(!att3.isInZone(pos)) {
+				moveToZoneX(att3, map);
+				if(!att3.isInYZone(pos))
+					teleportUp();
+				pos = getMinimapPosition(map);
+			}
+			rebuff(.75);
+			return position + 1;
+		case 5:
+		case 3:
+			Zone att4 = new Zone(new MaplePoint(184,37), new MaplePoint(190,44));
+			while(!att4.isInXZone(pos)) {
+				moveToZoneX(att4, map);
+				if(!att4.isInYZone(pos))
+					teleportUp();
+				pos = getMinimapPosition(map);
+			}
+			return position + 1;
+		case 4:
+			Zone att5 = new Zone(new MaplePoint(218,37), new MaplePoint(225,44));
+			while(!att5.isInXZone(pos)) {
+				moveToZoneX(att5, map);
+				if(!att5.isInYZone(pos))
+					teleportUp();
+				pos = getMinimapPosition(map);
+			}
+			return position + 1;
+		case 9:
+			return 0;
+		}
+		return 0;
+	}
+	public int jrWraithMovement(int position, MinimapData map) throws IOException {
+		MaplePoint pos = getMinimapPosition(map);
+		switch(position){
+		case 7:
+		case 0:
+			Zone att1 = new Zone(new MaplePoint(54,37), new MaplePoint(58,42));
+			while(!att1.isInZone(pos)) {
+				moveToZoneX(att1, map);
+				if(!att1.isInYZone(pos))
+					teleportUp();
+				pos = getMinimapPosition(map);
+			}
+			return position + 1;
+		case 6:
+		case 1:
+			Zone att2 = new Zone(new MaplePoint(109,43), new MaplePoint(114,48));
+			while(!att2.isInXZone(pos)) {
+				moveToZoneX(att2, map);
+//				if(!att2.isInYZone(pos[1])){
+//					if(pos[1] >= att2.getTopBound()) {
+//						teleportUp();
+//					} else {
+//						jumpDown();
+//					}
+//				}
+				rebuff(.75);
+				pos = getMinimapPosition(map);
+			}
+			return position + 1;
+		case 5:
+		case 2:
+			Zone att3 = new Zone(new MaplePoint(142,37), new MaplePoint(148,42));
+			while(!att3.isInXZone(pos)) {
+				moveToZoneX(att3, map);
+				if(!att3.isInYZone(pos)){
+					if(pos.y >= att3.getTopBound()) {
+						teleportUp();
+					} else {
+						jumpDown();
+					}
+				}
+					
+				pos = getMinimapPosition(map);
+			}
+			return position + 1;
+		case 4:
+		case 3:
+			Zone att4 = new Zone(new MaplePoint(200,37), new MaplePoint(206,42));
+			while(!att4.isInXZone(pos)) {
+				moveToZoneX(att4, map);
+				if(!att4.isInYZone(pos)){
+					if(pos.y >= att4.getTopBound()) {
+						teleportUp();
+					} else {
+						jumpDown();
+					}
+				}
+				pos = getMinimapPosition(map);
+			}
+			return position + 1;
+		case 8:
+			return 0;
+		}
 		return 0;
 	}
 }
