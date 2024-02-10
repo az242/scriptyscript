fe030a14f21648f74776f0fc17fb58cf19658850
Andrew Zhu <az242@rutgers.edu>
2024-02-09 20:32:29 -0500

    fixed runner

--- src/testrobot/runner.java
+++ src/testrobot/runner.java
@@ -11,19 +11,20 @@ public class runner {
 			Screen bishop = new Screen("bishop","names/botCleric.png", true);
 			Screen hermit = new Screen("hermit","names/hermitName.png", false);
 			Screen spearman = new Screen("spearman","names/lifedistroyname.png", false);
-			BaseBot bot = new Leafre(new Robot(), new Screen[]{
-					hermit, spearman},"royals");
-//			bot.enableSkill("Holy Symbol");
-//			bot.enableSkill("Invincible");
-//			bot.enableSkill("Magic Gaurd");
-//			bot.enableSkill("Maple Warrior");
+			BaseBot bot = new Singapore(new Robot(), new Screen[]{
+					bishop,hermit},"royals");
+			bot.enableSkill("Holy Symbol");
+			bot.enableSkill("Invincible");
+			bot.enableSkill("Magic Gaurd");
+			bot.enableSkill("Maple Warrior");
 //			bot.enableSkill("Magic");
 //			bot.enableSkill("Hyperbody");
 //			bot.enableSkill("Spearman Pot");
-//			bot.enableSkill("Haste");
-//			bot.enableSkill("Hermit Pot");
-			bot.swapMapleScreen(bot.getScreen("hermit"));
-//			bot.leech(8, bot.getMap("skele2"));
+			bot.enableSkill("Mesos Up");
+			bot.enableSkill("Haste");
+			bot.enableSkill("Hermit Pot");
+			bot.swapMapleScreen(bot.getScreen("bishop"));
+			bot.leech(8, bot.getMap("ulu2"));
 //			bot.rebuff(.5);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
